ad6ad11c789dfe79f7548b863f24397f8796394b
bardonfro <59987699+bardonfro@users.noreply.github.com>
2021-08-16 21:37:43 -0600

    Page layout

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import * as dommy from './dommy.js';
 import * as storage from './storage.js';
 import footer from './footer.js';
+import './style.scss';
 
-const x = dommy.el('h1.temp-title',"FOCUS");
-const y = dommy.el('h2.temp-subtitle',"A Task Manager For The Rest Of Us");
+const pageWrapper = dommy.el("div#page-wrapper");
+const header = dommy.el("div#header","Header");
+const contentWrapper = dommy.el("div.#content");
+const workspaceWrapper = dommy.el("div#workspace");
+const paneSelection = dommy.el("div#pane-selector");
 
-dommy.appendChildren(document.body,x,y,footer());
 
+
+
+
+dommy.appendChildren(contentWrapper,workspaceWrapper, paneSelection)
+dommy.appendChildren(pageWrapper,header,contentWrapper,footer());
+dommy.appendChildren(document.body,pageWrapper);
 
 storage.init();
-
-
-storage.remove("004");
-
 storage.show();
 
+ 
